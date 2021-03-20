48df9d589d5f96b1129c536bdfcfee945a6d5043
Torodoron <79978114+Torodoron@users.noreply.github.com>
2021-03-20 13:10:16 +0900

    20210319

---
+++ back1330.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class back1330 {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		if ( a > b)
+			System.out.println(">");
+		else if ( a < b)
+			System.out.println("<");
+		else
+			System.out.println("==");
+		
+		s.close();
+	}
+}
