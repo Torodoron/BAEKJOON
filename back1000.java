d88f74170b828ab901218d472ad4a652341f40e1
Torodoron <79978114+Torodoron@users.noreply.github.com>
2021-03-20 13:08:56 +0900

    20210319

---
+++ back1000.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+class back1000 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println((a+b));
+		scanner.close();
+
+	}
+}
