5aec654762fbdc9a8141334eb4ede9130d3d1f89
Torodoron <79978114+Torodoron@users.noreply.github.com>
2021-03-20 13:09:59 +0900

    20210319

---
+++ back10869.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class back10869 {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		s.close();
+	}
+}
